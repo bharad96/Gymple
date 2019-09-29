499ddbcd4f135c57753bc5efbf022ba4883f3786
Bharad <bharadwaj6991@gmail.com>
2019-09-29 16:45:07 +0800

    Added Floating Action Button
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

---
+++ app/src/main/java/com/example/android/gymple/MomentUploadActivity.java
@@ -0,0 +1,19 @@
+package com.example.android.gymple;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MomentUploadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_moment_upload);
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+    }
+}
--- app/src/main/java/com/example/android/gymple/MomentsFragment.java
+++ app/src/main/java/com/example/android/gymple/MomentsFragment.java
@@ -12,6 +12,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 
@@ -37,21 +38,38 @@ public class MomentsFragment extends Fragment {
         momentsArr.add(new Moment("User 1", "Second Workout", "test"));
         momentsArr.add(new Moment("User 2", "First Workout", "test"));
 
-        recyclerView = (RecyclerView) rootView.findViewById(R.id.recycler_view);
-
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
-
 //        recyclerView.setHasFixedSize(true);
 
+
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.recycler_view);
         // use a linear layout manager
-        layoutManager = new LinearLayoutManager(getActivity());
-        recyclerView.setLayoutManager(layoutManager);
+//        layoutManager = new LinearLayoutManager(getActivity());
+//        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
         // specify an adapter (see also next example)
         momentAdapter = new MomentsAdapter(momentsArr, getActivity());
         recyclerView.setAdapter(momentAdapter);
 
+        FloatingActionButton fab = rootView.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
         return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        //TODO keep recycler view here so that newly submitted moments show up
+
     }
 }
